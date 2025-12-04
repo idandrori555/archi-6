f48e2ebcee3d932e170dc3f8b88f25f942dbc59f
idan <idandrori555@gmail.com>
2025-12-04 17:38:58 +0200

    improved, need to do printing

--- ex6-1.asm
+++ ex6-1.asm
@@ -1,63 +1,68 @@
 org 100h
 
+jmp start
+
 CONTROL_PORT equ 127
 DATA_PORT equ 125
-CRITICAL_TEMP equ 60
+ZERO_ASCII equ '0'
 
-jmp start
+target_temp db 60
 
 inc_temp:
     in al, DATA_PORT
-    inc al
-    out DATA_PORT, al
-    jmp temp_loop
+    inc [target_temp]
+
+    jmp start
 
 dec_temp:
     in al, DATA_PORT
-    dec al
-    out DATA_PORT, al
-    jmp temp_loop
+    dec [target_temp]
+
+    jmp start
+
+    
+print_num_loop:    
+    cmp dl, 0
+    jbe print_num_end
+    
+    mov dl, bl
+    
+    mov ah, 0x2
+    int 21h
+    
+print_num_end:    
+    pop bp
+    ret 2
+ENDP   
+
 
 check_key_pressed:
     mov ah, 0x6
     mov dl, 0xFF
     int 21h ; syscall to check if key was pressed
-    jz temp_loop ; check if no key was pressed
+    jz start ; check if no key was pressed
     
     cmp al, '+'
     je inc_temp
     
     jmp dec_temp
 
-print_temp:
-    in al, DATA_PORT
-    mov bl, al
-    
-    mov dl, 'A' ; char to print
-    mov ah, 0x2 ; print char
-    int 21h ; syscall
-
 start:
-    mov al, 0x1
-    out CONTROL_PORT, al ; turn on fire
-    jmp temp_loop
-    
-lower_temp:
+    jmp sustain_temp
+
+turn_off_fire:
     mov ax, 0x0
-lower_temp_loop:
-    out CONTROL_PORT, ax
-    in al, DATA_PORT
-    cmp al, CRITICAL_TEMP
-    jae lower_temp_loop
+    out CONTROL_PORT, ax ; turn off fire
+
+sustain_temp:
+    in ax, DATA_PORT
+    cmp al, [target_temp]
+    jae turn_off_fire
     
-back_on:
     mov ax, 0x1
-    out CONTROL_PORT, ax    
-temp_loop:
-    in al, DATA_PORT
-    cmp al, CRITICAL_TEMP
-    jae lower_temp
-    jmp check_key_pressed ; check key + jump back
+    out CONTROL_PORT, ax ; turn on fire
+    
+    jmp check_key_pressed
 
 end:
 xor ah, ah
